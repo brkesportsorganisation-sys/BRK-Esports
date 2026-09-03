6e0dd4087948fd7664340628524696542473d3b2
BRK Esports <brkesportsorganisation-sys@users.noreply.github.com>
2026-09-04 00:12:56 +0600

    perf: set images unoptimized to prevent Vercel fast origin bandwidth limit

--- next.config.mjs
+++ next.config.mjs
@@ -94,6 +94,7 @@ const nextConfig = {
   },
 
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 2592000, // 30 days
     deviceSizes: [360, 480, 640, 750, 828, 1080, 1200, 1920],
